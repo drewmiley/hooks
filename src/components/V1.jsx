b9c19217560e2ea4ddb06ec59955c969415308eb
Drew Miley <drewmiley92@gmail.com>
2018-12-27 00:53:41 +0000

    Component V1 complete

--- src/components/V1.jsx
+++ src/components/V1.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
 
 export default class V1 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+        this.onChange = this.onChange.bind(this);
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ value: e.target.value });
+    }
+
+    onClick() {
+        console.log(this.state.value);
+    }
+
     render() {
         return (
             <>
                 <h2>Component V1</h2>
                 <p>
-                    <input type='text' placeholder='Input'></input>
-                    <button onClick={() => console.log('value')}>Log Value</button>
+                    <input type='text' placeholder='Input' value={this.state.value} onChange={this.onChange}></input>
+                    <button onClick={this.onClick}>Log Value</button>
                 </p>
             </>
         );
