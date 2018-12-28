62ef212f9a53fbe7139e3151d9dc4788d2b84c89
Drew Miley <drewmiley92@gmail.com>
2018-12-28 22:59:04 +0000

    Side-by-side layout

--- src/AppContainer.jsx
+++ src/AppContainer.jsx
@@ -10,32 +10,32 @@ class App extends Component {
     render() {
         return (
             <>
-                <Components.V0 {...this.props} />
-                <Hooks.V0 {...this.props} />
-                <Components.V1 />
-                <Hooks.V1 />
-                <Components.V2 {...this.props} />
-                <Hooks.V2 {...this.props} />
-                <Components.V3 {...this.props} />
-                <Hooks.V3 {...this.props} />
-                <Components.V4 />
-                <Hooks.V4 />
-                <Components.V5 {...this.props} />
-                <Hooks.V5 {...this.props} />
-                <Components.V6 {...this.props} />
-                <Hooks.V6 {...this.props} />
-                { this.props.count <= 5 && <Components.V7 {...this.props} />}
-                { this.props.count <= 5 && <Hooks.V7 {...this.props} />}
-                <Components.V8 />
-                <Hooks.V8 />
-                <Components.V9 />
-                <Hooks.V9 />
-                { this.props.count <= 5 && <Components.V10 {...this.props} />}
-                { this.props.count <= 5 && <Hooks.V10 {...this.props} />}
-                <Components.V11 {...this.props} />
-                <Hooks.V11 {...this.props} />
-                <Components.V12 {...this.props} />
-                <Hooks.V12 {...this.props} />
+                <div className='half-width'><Components.V0 {...this.props} /></div>
+                <div className='half-width'><Hooks.V0 {...this.props} /></div>
+                <div className='half-width'><Components.V1 /></div>
+                <div className='half-width'><Hooks.V1 /></div>
+                <div className='half-width'><Components.V2 {...this.props} /></div>
+                <div className='half-width'><Hooks.V2 {...this.props} /></div>
+                <div className='half-width'><Components.V3 {...this.props} /></div>
+                <div className='half-width'><Hooks.V3 {...this.props} /></div>
+                <div className='half-width'><Components.V4 /></div>
+                <div className='half-width'><Hooks.V4 /></div>
+                <div className='half-width'><Components.V5 {...this.props} /></div>
+                <div className='half-width'><Hooks.V5 {...this.props} /></div>
+                <div className='half-width'><Components.V6 {...this.props} /></div>
+                <div className='half-width'><Hooks.V6 {...this.props} /></div>
+                { this.props.count <= 5 && <div className='half-width'><Components.V7 {...this.props} /></div>}
+                { this.props.count <= 5 && <div className='half-width'><Hooks.V7 {...this.props} /></div>}
+                <div className='half-width'><Components.V8 /></div>
+                <div className='half-width'><Hooks.V8 /></div>
+                <div className='half-width'><Components.V9 /></div>
+                <div className='half-width'><Hooks.V9 /></div>
+                { this.props.count <= 5 && <div className='half-width'><Components.V10 {...this.props} /></div>}
+                { this.props.count <= 5 && <div className='half-width'><Hooks.V10 {...this.props} /></div>}
+                <div className='half-width'><Components.V11 {...this.props} /></div>
+                <div className='half-width'><Hooks.V11 {...this.props} /></div>
+                <div className='half-width'><Components.V12 {...this.props} /></div>
+                <div className='half-width'><Hooks.V12 {...this.props} /></div>
             </>
         )
     }
