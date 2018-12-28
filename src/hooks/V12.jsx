383ca5fa0383dd88744fa71101fde9e7f2222662
Drew Miley <drewmiley92@gmail.com>
2018-12-28 22:41:36 +0000

    Implements V12 Hook

--- src/hooks/V12.jsx
+++ src/hooks/V12.jsx
@@ -1,21 +1,27 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
-const getProduct = (count, random) => {
-    for (let i = 0; i < 100; i++) {
-        console.log('hook computing expensive function')
+const getProduct = (count, random, loops) => {
+    for (let i = 0; i < loops; i++) {
+        console.log(`hook computing expensive function looping ${loops} times`);
     }
     return count * random;
 }
 
 const V11 = props => {
+    const [value, setValue] = useState(NaN);
     const [trigger, setTrigger] = useState(false);
     const [loops, setLoops] = useState(100);
-    const product = useCallback(() => getProduct(props.count, props.random), [trigger]);
+    const product = useCallback(() => getProduct(props.count, props.random, loops), [loops]);
+
+    useEffect(() => {
+        setValue(product());
+    }, [trigger])
+
     return (
         <>
             <h2>Hooks V12</h2>
             <p>
-                <span>Count * Random: {product()}</span>
+                <span>Count * Random: {value}</span>
             </p>
             <p>
                 <span>Loops: <input type="number" name="quantity" value={loops} onChange={(e) => setLoops(parseInt(e.target.value))} min="1" max="1000000" /></span>
