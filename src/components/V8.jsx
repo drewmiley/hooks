0a817f7aa42f549d9772e86b8ad47ed0a6824604
Drew Miley <drewmiley92@gmail.com>
2018-12-28 13:45:38 +0000

    Adds V8 component and README

--- src/components/V8.jsx
+++ src/components/V8.jsx
@@ -3,31 +3,24 @@ import React, { Component } from 'react'
 export default class V8 extends Component {
     constructor(props) {
         super(props);
-        this.state = { count: 0 };
-        this.setCount = this.setCount.bind(this);
+        this.state = { users: [] };
     }
 
-    setCount(count) {
-        this.setState({ count });
-    }
-
-    componentDidMount() {
-        console.log('component did mount');
-        this.setCount(20);
-    }
-
-    componentDidUpdate() {
-        console.log(`component did update`);
+    async componentDidMount() {
+        const response = await fetch('https://randomuser.me/api/?results=2');
+        const json = await response.json();
+        this.setState({ users: json.results });
     }
 
     render() {
         return (
             <>
-                <h2>Component V4</h2>
-                <p>
-                    <span>Count: {this.state.count}</span>
-                    <span><button onClick={() => this.setCount(this.state.count + 1)}>Increment</button></span>
-                </p>
+                <h2>Components V8</h2>
+                <ul>
+                    {this.state.users.map((user, i) => (
+                        <li key={i}>{user.name.title} {user.name.first} {user.name.last}</li>
+                    ))}
+                </ul>
             </>
         );
     }
