371e3451ed2beb9c07d2335ba5a5e46aa820170d
Drew Miley <drewmiley92@gmail.com>
2018-12-28 14:22:19 +0000

    Slight cleanup of App

--- src/AppContainer.jsx
+++ src/AppContainer.jsx
@@ -8,30 +8,32 @@ import { mapDispatchToProps } from './ducks/actions';
 
 class App extends Component {
     render() {
-        return <div>
-            <Components.V0 {...this.props} />
-            <Hooks.V0 {...this.props} />
-            <Components.V1 />
-            <Hooks.V1 />
-            <Components.V2 {...this.props} />
-            <Hooks.V2 {...this.props} />
-            <Components.V3 {...this.props} />
-            <Hooks.V3 {...this.props} />
-            <Components.V4 />
-            <Hooks.V4 />
-            <Components.V5 {...this.props} />
-            <Hooks.V5 {...this.props} />
-            <Components.V6 {...this.props} />
-            <Hooks.V6 {...this.props} />
-            { this.props.count <= 5 && <Components.V7 {...this.props} />}
-            { this.props.count <= 5 && <Hooks.V7 {...this.props} />}
-            <Components.V8 />
-            <Hooks.V8 />
-            <Components.V9 />
-            <Hooks.V9 />
-            { this.props.count <= 5 && <Components.V10 {...this.props} />}
-            { this.props.count <= 5 && <Hooks.V10 {...this.props} />}
-        </div>
+        return (
+            <>
+                <Components.V0 {...this.props} />
+                <Hooks.V0 {...this.props} />
+                <Components.V1 />
+                <Hooks.V1 />
+                <Components.V2 {...this.props} />
+                <Hooks.V2 {...this.props} />
+                <Components.V3 {...this.props} />
+                <Hooks.V3 {...this.props} />
+                <Components.V4 />
+                <Hooks.V4 />
+                <Components.V5 {...this.props} />
+                <Hooks.V5 {...this.props} />
+                <Components.V6 {...this.props} />
+                <Hooks.V6 {...this.props} />
+                { this.props.count <= 5 && <Components.V7 {...this.props} />}
+                { this.props.count <= 5 && <Hooks.V7 {...this.props} />}
+                <Components.V8 />
+                <Hooks.V8 />
+                <Components.V9 />
+                <Hooks.V9 />
+                { this.props.count <= 5 && <Components.V10 {...this.props} />}
+                { this.props.count <= 5 && <Hooks.V10 {...this.props} />}
+            </>
+        )
     }
 };
 
